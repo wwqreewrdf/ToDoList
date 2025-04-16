f2a42c1b0e5f71cf2c71fce3410cced3c57631ae
LOLsite <farfromsouls@gmail.com>
2025-04-17 03:44:13 +0700

    bot init

---
+++ telegrambot/main.py
@@ -0,0 +1,24 @@
+import os
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from secret import token
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
