7b47dbece441fe945f804eaf34d5d449787bc9b4
farfromsouls <farfromsouls@gmail.com>
2025-04-28 20:04:44 +0700

    migrated to postgre

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
