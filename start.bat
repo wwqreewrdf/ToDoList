0fa15b9c63816e8872e381859f9f0a573f3439e5
farfromsouls <farfromsouls@gmail.com>
2025-05-06 20:34:06 +0700

    updated start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,13 @@
 @echo off
 
+if not exist "%~dp0frontend/node_modules" (
+    start "NodeModulesInstall" cmd /k "cd /d %~dp0frontend && npm i"
+)
+
+if not exist "%~dp0backend/venv" (
+    start "PythonModulesInstall" cmd /k "cd /d %~dp0backend && venv\Scripts\activate && pip install -r requirements.txt"
+)
+
 start "Backend" cmd /k "cd /d %~dp0backend && venv\Scripts\activate && python manage.py runserver"
 start "Frontend" cmd /k "cd /d %~dp0frontend && npm start"
 
