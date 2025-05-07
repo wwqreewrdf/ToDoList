be356cd57c310f09cf3e54feea48bbeb577a1c97
wwqreewrdf <artembogdanov44747@gmail.com>
2025-05-07 23:06:59 +0700

    ура первый комит

---
+++ telegrambot/data.py
@@ -0,0 +1,38 @@
+async def check_id(id):
+
+
+async def set_id(id):
+
+
+async def set_login(id):
+
+
+async def get_login(id): 
+    return login   
+
+
+async def set_LBM(id): #last bot message
+
+
+async def get_LBM(id):
+
+
+async def get_tokens(id):
+    #реквест на токены , получаем одним сообщ оба токена потом проста сплит по пробелу или чо там будет
+    #сразу закрепляем их за айдишником
+
+
+async def get_access(id):
+
+
+async def get_refresh(id):    
+   
+
+
+
+
+
+
+
+
+    
--- telegrambot/main.py
+++ telegrambot/main.py
@@ -2,19 +2,62 @@ import os
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import FSInputFile
+from aiogram.filters.command import Command
 
-from secret import token
+from data import *
+
+
 
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token)
+bot = Bot("7799584237:AAG_XF_Ol3MjPkagzlMIo_AleQ4bzEuH56c")
 dp = Dispatcher()
 
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("")
+    await message.answer("Здравствуйте, введите логин")
+
+
+@dp.message(F.text)
+async def handler(message: types.Message):
+    id = message.from_user.id
+    text = message.text
+
+
+    lastBM = await get_LBM(id)
+    
+    if await check_id(id) == 0 or lastBM == "Неправильно введен логин":
+        await set_id(id)
+
+        #реквест на апи на существование логина вапще 
+        #Сценарий что существует(функция возращ 1)
+        if request == 1:
+            await set_login(id)
+
+    else:
+        await bot.send_message(id, "Неправильно введен логин, попробуйте еще раз")
+        await set_LBM(id, "логин")
+
+
+
+     
+
+    if lastBM == None or lastBM == "неверный пароль":
+        await bot.send_message(id, "введите пароль")
+        await set_LBM(id, "пароль")
+        #реквест на апи на правильность пароля
+        if request == 1:
+            login = get_login(id)
+            get_tokens(login)
+        else:
+            await bot.send_message(id, "Неправильно введен пароль, попробуйте еще раз")
+            await set_LBM(id, "неверный пароль")
+
+
+    
 
 
 async def main():
